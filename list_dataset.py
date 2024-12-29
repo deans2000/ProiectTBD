c02b66294dad6ec07c0133f414e87412861cbb93
Slatinat Dean <deanslatinat@yahoo.com>
2024-12-29 18:09:33 +0200

    Obiectivul 1

---
+++ list_dataset.py
@@ -0,0 +1,9 @@
+import os
+
+# Path către director
+dataset_path = r"C:\Users\deans\.cache\kagglehub\datasets\netflix-inc\netflix-prize-data\versions\2"
+
+# Listare fișiere
+files = os.listdir(dataset_path)
+print(files)
+
